08ac929eefc564ce9dc9f4aa59eb8588eeb32797
agustiinwilliamsoninacap <agustin.williamson@inacapmail.cl>
2025-11-05 20:41:12 +0000

    ventas arreglo

--- mermeladas_django_con_produccion/mermeladas_django/mermeladas/settings.py
+++ mermeladas_django_con_produccion/mermeladas_django/mermeladas/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ventas',
+    'mermeladas_django.ventas',
 ]
 
 MIDDLEWARE = [
