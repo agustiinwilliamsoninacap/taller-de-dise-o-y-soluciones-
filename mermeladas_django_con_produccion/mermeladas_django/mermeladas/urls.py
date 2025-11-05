af0783a89e0f200f7a322d9258edc1f2d1a1b6b9
agustiinwilliamsoninacap <agustin.williamson@inacapmail.cl>
2025-11-05 20:53:19 +0000

    urls.py corregida

--- mermeladas_django_con_produccion/mermeladas_django/mermeladas/urls.py
+++ mermeladas_django_con_produccion/mermeladas_django/mermeladas/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ventas.urls')),
+    path('', include('mermeladas_django.ventas.urls')),  # <- ruta completa
 ]
