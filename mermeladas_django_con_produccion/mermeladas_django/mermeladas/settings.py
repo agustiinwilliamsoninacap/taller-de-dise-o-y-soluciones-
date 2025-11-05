5199545ee116f200e0ec1b7aa767bf49e88d2f86
agustiinwilliamsoninacap <agustin.williamson@inacapmail.cl>
2025-11-05 21:16:20 +0000

    s

--- mermeladas_django_con_produccion/mermeladas_django/mermeladas/settings.py
+++ mermeladas_django_con_produccion/mermeladas_django/mermeladas/settings.py
@@ -74,3 +74,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # importante para Render
 
 # Tipo de campo automático
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "root": {"handlers": ["console"], "level": "ERROR"},
+}
