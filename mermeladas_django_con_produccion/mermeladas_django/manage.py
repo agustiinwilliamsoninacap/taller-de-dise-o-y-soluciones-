ce1e29abc0c6b555f8b7c3b3f3c1a704350e5e6f
agustiinwilliamsoninacap <agustin.williamson@inacapmail.cl>
2025-11-10 14:45:18 -0300

    sera

--- mermeladas_django_con_produccion/mermeladas_django/manage.py
+++ mermeladas_django_con_produccion/mermeladas_django/manage.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
-import os
-import sys
-
+import os, sys
 def main():
-    # apunta al settings correcto dentro del paquete
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "mermeladas_django.mermeladas.settings",
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mermeladas.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
 if __name__ == "__main__":
     main()
